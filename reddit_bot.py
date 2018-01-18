043af14008bb2086656254550ad0a1e67f7d2683
Jamie <jastley92@gmail.com>
2018-01-18 19:11:38 +1100

    Successfully finds missing limbs on r/test

--- reddit_bot.py
+++ reddit_bot.py
@@ -1,1 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*- 
+
 import praw
+import config
+
+shrug = '¯\_(ツ)_/¯'
+
+def bot_login():
+    r = praw.Reddit(username = config.username,
+			password = config.password,
+			client_id = config.client_id,
+			client_secret = config.client_secret,
+			user_agent = "Jizzy_Gillespie92 limb retrieval bot")
+    return r
+
+def run_bot(r):
+	print shrug
+	for comment in r.subreddit('test').comments(limit=105):
+		if shrug.decode('utf-8') in comment.body:
+			print "shrug found!", shrug
+			comment.reply("You dropped this \ ")
+r = bot_login()
+run_bot(r)
