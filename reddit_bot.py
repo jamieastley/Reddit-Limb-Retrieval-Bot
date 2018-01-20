77f88bb9f49f4d0e4f1843055594438fa3ca5e46
Jamie <jastley92@gmail.com>
2018-01-21 02:33:15 +1100

    try/except's added

--- reddit_bot.py
+++ reddit_bot.py
@@ -19,35 +19,52 @@ def bot_login():
 
 def run_bot(r):
 	subreddit = r.subreddit('all')
-	comments = subreddit.stream.comments()
-	for comment in comments:
-		text = comment.body
-		author = comment.author
+	try:
+		comments = subreddit.stream.comments()
+		for comment in comments:
+			text = comment.body
+			author = comment.author
 
-		if shrug.decode('utf-8') in text: #decode required for unicode characters
-		#create/open log.txt
-			try:
-				file = open("log.txt", "a")
-			except IOError:
-				file = open("log.txt", "w")
+			if shrug.decode('utf-8') in text: #decode required for unicode characters
+			#create/open log.txt
+				try:
+					file = open("log.txt", "a")
+				except IOError:
+					file = open("log.txt", "w")
 
-			file.write(str(author) + shrug + '\n')
-			file.close()
+				file.write(str(author) + '\n')
+				file.close()
 
-			# print "Missing limb found in: ", "(",author,")", text
-			comment.reply("You dropped this \ " + footer.footerComment)
+				# print "Missing limb found in: ", "(",author,")", text
+				try:
+					comment.reply("You dropped this \ " + footer.footerComment)
+				except:
+					file = open("log.txt", "w")
+					file.write("Exception caught!" + '\n') #TODO error details
+					file.close()
 			
-		
-		elif shoulders.decode('utf-8') in text:
-			try:
-				file = open("log.txt", "a")
-			except IOError:
-				file = open("log.txt", "w")
+			elif shoulders.decode('utf-8') in text:
+				try:
+					file = open("log.txt", "a")
+				except IOError:
+					file = open("log.txt", "w")
 
-			file.write(str(author) + shoulders + '\n')
-			file.close()
-			# print "Shoulders lost forever: ", "(",author,")", text
-			comment.reply("I have retrieved these for you _ _" + footer.footerComment)
+				file.write(str(author) + '\n')
+				file.close()
+				# print "Shoulders lost forever: ", "(",author,")", text
+				try:
+					comment.reply("I have retrieved these for you _ _" + footer.footerComment)
+				except:
+					file = open("log.txt", "w")
+					file.write("Exception caught!" + '\n') #TODO error details
+					file.close()					
+	except:
+		try:
+			file = open("log.txt", "a")
+		except IOError:
+			file = open("log.txt", "w")
+			file.write("Exception caught!" + '\n') #TODO error details
+		file.close()
 
 r = bot_login()
 run_bot(r)
