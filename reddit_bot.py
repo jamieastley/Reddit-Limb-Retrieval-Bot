c35af1245fc9e99b4d8d8e5f08d06ef1a1de34f6
Jamie <jastley92@gmail.com>
2018-01-24 02:56:50 +1100

    main function rewrite, better exception logging

--- reddit_bot.py
+++ reddit_bot.py
@@ -4,6 +4,7 @@
 import praw
 import config
 import footer
+from datetime import datetime
 
 shrug = '¯\_(ツ)_/¯'
 shoulders = '¯\\\_(ツ)_/¯'
@@ -14,57 +15,53 @@ def bot_login():
 			password = config.password,
 			client_id = config.client_id,
 			client_secret = config.client_secret,
-			user_agent = "LimbRetrieval-Bot by u/Jizzy_Gillespie92")
+			user_agent = "u/LimbRetrieval-Bot")
     return r
 
 def run_bot(r):
 	subreddit = r.subreddit('all')
-	try:
-		comments = subreddit.stream.comments()
-		for comment in comments:
-			text = comment.body
-			author = comment.author
+	# try:
+	comments = subreddit.stream.comments()
+	for comment in comments:
+		text = comment.body
+		author = comment.author
+		sub = str(comment.subreddit)
 
-			if shrug.decode('utf-8') in text: #decode required for unicode characters
-			#create/open log.txt
-				try:
-					file = open("log.txt", "a")
-				except IOError:
-					file = open("log.txt", "w")
+		if shrug.decode('utf-8') in text: #decode required for unicode characters
+		#create/open log.txt
+			try:
+				file = open("log.txt", "a")
+			except IOError:
+				file = open("log.txt", "w")
 
-				file.write(str(author) + '\n')
-				file.close()
+			file.write(str(author) + '(r/' + sub + ')' '\n')
+			file.close()
 
-				# print "Missing limb found in: ", "(",author,")", text
-				try:
-					comment.reply("You dropped this \ " + footer.footerComment)
-				except:
-					file = open("log.txt", "w")
-					file.write("Exception caught!" + '\n') #TODO error details
-					file.close()
+			# print "Missing limb found in: ", "(",author,")", text
+			comment.reply("You dropped this \ " + footer.footerComment)
 			
-			elif shoulders.decode('utf-8') in text:
-				try:
-					file = open("log.txt", "a")
-				except IOError:
-					file = open("log.txt", "w")
+		
+		elif shoulders.decode('utf-8') in text:
+			try:
+				file = open("log.txt", "a")
+			except IOError:
+				file = open("log.txt", "w")
 
-				file.write(str(author) + '\n')
-				file.close()
-				# print "Shoulders lost forever: ", "(",author,")", text
-				try:
-					comment.reply("I have retrieved these for you _ _" + footer.footerComment)
-				except:
-					file = open("log.txt", "w")
-					file.write("Exception caught!" + '\n') #TODO error details
-					file.close()					
-	except:
-		try:
-			file = open("log.txt", "a")
-		except IOError:
-			file = open("log.txt", "w")
-			file.write("Exception caught!" + '\n') #TODO error details
+			file.write(str(author) + '(r/' + sub + ')' '\n')
+			file.close()
+			# print "Shoulders lost forever: ", "(",author,")", text
+			comment.reply("I have retrieved these for you _ _" + footer.footerComment)
+
+while True:
+	try:
+		r = bot_login()
+		run_bot(r)
+		
+	except Exception as ex:
+		err = str(ex)
+		errTime = str(datetime.now())
+		file = open("log.txt", "a")
+		# file = open("log.txt", "w")
+		file.write("Exception caught! : " + err + ", " + errTime + "\n") 
 		file.close()
-
-r = bot_login()
-run_bot(r)
+		continue
